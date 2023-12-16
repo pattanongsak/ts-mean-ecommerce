ca5b7efc73e0f0422e8b3700c40429f7940ce85c
pattanongsak <18730626+pattanongsak@users.noreply.github.com>
2023-12-16 16:11:23 +0700

    backend fetch products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Container, Navbar, Nav, Row, Col } from "react-bootstrap";
+import { sampleProducts } from "./data";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <header>TS Ecommerce</header>
-      <main></main>
-      <footer>All right reserved</footer>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>ts Ecommerce</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
+      <main>
+        <Container className="mt-3">
+          <Outlet />
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center">All right reserved</div>
+      </footer>
     </div>
   );
 }
